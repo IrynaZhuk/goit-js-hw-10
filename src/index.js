d4de15460643165befd9109afa4ab2f80977a609
IrynaZhuk <irynazh94@gmail.com>
2022-06-24 21:15:11 +0300

    1st commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const debounce = require('lodash.debounce');
+
+const DEBOUNCE_DELAY = 300;
